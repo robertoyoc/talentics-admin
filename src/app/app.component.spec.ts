94f6c4960bb57106c5f22d150cc4e820f85d68fa
Roberto Yoc <roberto.yoc@energyza.energy>
2019-05-13 17:49:41 -0500

    testing ready to code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,44 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MatIconModule } from '@angular/material';
+import { By } from '@angular/platform-browser';
+import { RouterLinkWithHref } from '@angular/router';
 
 
 describe('AppComponent', () => {
+  let fixture;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
       imports: [
+        MatIconModule,
         RouterTestingModule
       ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+  it('should have a logout button', () => {
+    const body: HTMLElement = fixture.nativeElement;
+    const p = body.querySelector('mat-icon');
+    expect(p.textContent).toEqual('call_made');
+  });
+  it('should have 1 router link', () => {
+    const debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+    expect(debugElements.length).toBe(1);
+
+  });
+  it('can logout', () => {
+    const debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+    const index = debugElements.findIndex(de => {
+      return de.attributes['routerLink'] === 'logout';
+    });
+    expect(index).toBeGreaterThan(-1);
+  });
 });
