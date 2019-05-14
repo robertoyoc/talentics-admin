3a803f996091035516fcfc8aaee7c39fd306e5f9
NissimBet <nissimbet123@outlook.com>
2019-05-14 15:34:40 -0500

    added kit styles and error fixed on componente guard

--- src/app/resolvers/component-resolver/component-resolver.guard.ts
+++ src/app/resolvers/component-resolver/component-resolver.guard.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Component } from 'src/app/models/component';
+import { Componente } from 'src/app/models/componente';
 import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ComponentResolverGuard implements Resolve<Component> {
+export class ComponentResolverGuard implements Resolve<Componente> {
   constructor(
     private db: AngularFirestore
   ) { }
   resolve(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<Component> | Promise<Component> | Component {
+    state: RouterStateSnapshot): Observable<Componente> | Promise<Componente> | Componente {
       const id = route.params.id;
-      const subscriber = this.db.doc<Component>(`components/${id}`).valueChanges();
+      const subscriber = this.db.doc<Componente>(`components/${id}`).valueChanges();
       return new Observable((obs) => {
-        subscriber.subscribe((item: Component) => {
+        subscriber.subscribe((item: Componente) => {
           obs.next(item);
           obs.complete();
         });
